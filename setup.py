ccb8df97ba82c24693f3af6db94d270eef4ef0b1
gcroci2 <crocioni.giulia@gmail.com>
2024-07-15 15:00:45 +0200

    add gymnasium last version

--- setup.py
+++ setup.py
@@ -1,42 +1,46 @@
-import re
-from setuptools import setup, find_packages
-import sys
 import os
+import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'neurogym'))
+from setuptools import find_packages, setup
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "neurogym"))
 from version import VERSION
 
-
 if sys.version_info.major != 3:
-    print('This Python is only compatible with Python 3, but you are running '
-          'Python {}. The installation will likely fail.'.format(sys.version_info.major))
+    print(
+        "This Python is only compatible with Python 3, but you are running "
+        "Python {}. The installation will likely fail.".format(sys.version_info.major)
+    )
 
 
 # Environment-specific dependencies.
 extras = {
-  'psychopy': ['psychopy'],
+    "psychopy": ["psychopy"],
 }
 
 # Meta dependency groups.
-extras['all'] = [item for group in extras.values() for item in group]
+extras["all"] = [item for group in extras.values() for item in group]
 
 # For developers
-extras['dev'] = extras['all'] + [
-    'jupyter', 'pytest', 'sphinx', 'sphinx_rtd_theme', 'sphinxcontrib.katex',
-    'nbsphinx'
+extras["dev"] = extras["all"] + [
+    "jupyter",
+    "pytest",
+    "sphinx",
+    "sphinx_rtd_theme",
+    "sphinxcontrib.katex",
+    "nbsphinx",
 ]
 
-setup(name='neurogym',
-      packages=[package for package in find_packages()
-                if package.startswith('neurogym')],
-      install_requires=[
-          'numpy',
-          'gym>=0.20.0, <0.25',
-          'matplotlib',
-      ],
-      extras_require=extras,
-      description='NeuroGym: Gym-style cognitive neuroscience tasks',
-      author='Manuel Molano, Guangyu Robert Yang, and contributors',
-      url='https://github.com/neurogym/neurogym',
-      version=VERSION)
-
+setup(
+    name="neurogym",
+    packages=[package for package in find_packages() if package.startswith("neurogym")],
+    install_requires=[
+        "numpy",
+        "gymnasium>=0.29.1" "matplotlib",
+    ],
+    extras_require=extras,
+    description="NeuroGym: Gymnasium-style cognitive neuroscience tasks",
+    author="Manuel Molano, Guangyu Robert Yang, and contributors",
+    url="https://github.com/ANNUBS/neurogym",
+    version=VERSION,
+)
